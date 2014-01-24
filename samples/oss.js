566d3847f87e984651319a370f4b7c36326ffd24
chylvina <chylvina@gmail.com>
2014-01-24 18:20:06 +0800

    deleteObjects works

--- samples/oss.js
+++ samples/oss.js
@@ -126,6 +126,7 @@ var oss = new AWS.OSS({
  });
  });*/
 
+/*
 oss.getObject({
     Bucket: 'chylvina',
     Key: 'test.json',
@@ -141,6 +142,46 @@ oss.getObject({
 
     console.log(200, data);
 
-    //console.log(data.Body.toString('utf8'));
+    console.log(data.Body.toString('utf8'));
+
+  });*/
+
+/*
+oss.deleteObject({
+    Bucket: 'chylvina',
+    Key: 'test.json'
+  },
+  function (err, data) {
+
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log(200, data);
+
+  });*/
+
+oss.deleteObjects({
+    Bucket: 'chylvina',
+    Delete: {
+      Objects: [
+        {
+          Key: '1.html'
+        },
+        {
+          Key: '2.html'
+        }
+      ]
+    }
+  },
+  function (err, data) {
+
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log(200, data);
 
   });
