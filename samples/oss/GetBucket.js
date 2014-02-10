a12efd6febd57c374b5743ef6055cf7662f46684
chylvina <chylvina@gmail.com>
2014-02-10 19:00:54 +0800

    change config

--- samples/oss/GetBucket.js
+++ samples/oss/GetBucket.js
@@ -1,15 +1,10 @@
 var ALY = require('../../index.js');
 
-// AWS.config.loadFromPath('../config.json');
+ALY.config.loadFromPath('../config.json');
 
 var oss = new ALY.OSS({
-  endpoint: 'http://oss-test.aliyun-inc.com',//'http://oss-cn-hangzhou.aliyuncs.com',
-  accessKeyId: 'pgvydev9bcbwpe3',//"DpqmTnnCVV45s7gP",
-  secretAccessKey: 'ZXJ1ZzZqbTdraWp4eTlkZWhmMGk=',//"wJT5OddEiVW0o3BqolssXmD5MYWTJv",
-  apiVersion: '2013-10-15',
-  httpOptions: {
-    //proxy: 'http://127.0.0.1:8888'
-  }
+  endpoint: 'http://oss-test.aliyun-inc.com',
+  apiVersion: '2013-10-15'
 });
 
 // -------------------------------
